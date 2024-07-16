46283368bd12944975c039113cd5dc6db5362107
marco-scocca <marcoscocca1991@gmail.com>
2024-07-16 09:15:23 +0200

    stampo su console le info dei personaggi

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
 const teams = [
     {
-        name: "Wayne Barnett", role: "Founder & CEO", img: " wayne-barnett-founder-ceo.jpg",
-        name: "Angela Caroll", role: "Chief Editor", img: "angela - caroll - chief - editor.jpg",
-        name: "Walter Gordon", role: "Office Manager", img: "walter - gordon - office - manager.jpg",
-        name: "Angela Lopez ", role: "Social Media Manager", img: "angela - lopez - social - media - manager.jpg",
-        name: "Scott Estrada ", role: "Developer", img: "scott - estrada - developer.jpg",
-        name: "Barbara Ramos ", role: "Graphic Designer", img: "barbara - ramos - graphic - designer.jpg",
-    }
-]
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        img: " wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        img: "angela - caroll - chief - editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        img: "walter - gordon - office - manager.jpg"
+    },
+    {
+        name: "Angela Lopez ",
+        role: "Social Media Manager",
+        img: "angela - lopez - social - media - manager.jpg"
+    },
+    {
+        name: "Scott Estrada ",
+        role: "Developer",
+        img: "scott - estrada - developer.jpg"
+    },
+    {
+        name: "Barbara Ramos ",
+        role: "Graphic Designer",
+        img: "barbara - ramos - graphic - designer.jpg"
+    },
+
+];
+
+for (let i = 0; i < teams.length; i++) {
+    console.log(`${teams[i].name} ${teams[i].role} ${teams[i].img}`)
+}
