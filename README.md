6fa3ae27ca51a27392cbf4dc52f6c1ddc3ff8469
marco-scocca <marcoscocca1991@gmail.com>
2024-07-16 09:04:21 +0200

    creo array

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ imposto nell'html un elemento per contenere le stringhe
 richiamo l'elemento nel js
 ciclo array
 inserisco nell'html la stringa generata
+
+Wayne Barnett - Founder & CEO - wayne-barnett-founder-ceo.jpg
+Angela Caroll	- Chief Editor	- angela-caroll-chief-editor.jpg
+Walter Gordon - Office Manager - walter-gordon-office-manager.jpg
+Angela Lopez	- Social Media Manager - angela-lopez-social-media-manager.jpg
+Scott Estrada	- Developer - scott-estrada-developer.jpg
+Barbara Ramos - Graphic Designer - barbara-ramos-graphic-designer.jpg
--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-let num = [1, 2, 3, 4];
-console.log(num);
-function changeNum(value) {
-    console.log(value);
-    value = 7;
-    return value;
-}
-
-let result = changeNum(num);
-console.log(result);
-console.log(nums);
+const teams = [
+    {
+        name: "Wayne Barnett", role: "Founder & CEO", img: " wayne-barnett-founder-ceo.jpg",
+        name: "Angela Caroll", role: "Chief Editor", img: "angela - caroll - chief - editor.jpg",
+        name: "Walter Gordon", role: "Office Manager", img: "walter - gordon - office - manager.jpg",
+        name: "Angela Lopez ", role: "Social Media Manager", img: "angela - lopez - social - media - manager.jpg",
+        name: "Scott Estrada ", role: "Developer", img: "scott - estrada - developer.jpg",
+        name: "Barbara Ramos ", role: "Graphic Designer", img: "barbara - ramos - graphic - designer.jpg",
+    }
+]
